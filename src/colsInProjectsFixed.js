e99092ecfdc45f41f0bc6669d9be0a34c72e2d1e
Gonzalez Martinez <orlandox.j.gonzalez.martinez@intel.com>
2020-10-07 17:55:36 -0500

    update 10/7/20

--- src/colsInProjectsFixed.js
+++ src/colsInProjectsFixed.js
@@ -1,19 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Row from "react-bootstrap/Row";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
 import { animated, useTrail } from 'react-spring';
-// import { Item } from "react-bootstrap/lib/Carousel";
 
 function ColsInProjectsFixed(props) {
-    // const url = "/school-projects.json";
-    // const _URL_freelancer = "/freelance-projects.json";
-useEffect(() => {
-    console.log('props projjects', props.projects);
-}, [])
-
-   
+  
 
     function hoverEffect(e) { /*hover scale effect on column img*/
         let element = e.target.nextSibling;
@@ -21,45 +14,38 @@ useEffect(() => {
         card.classList.toggle('hover-image');
     }
 
-    // const columnEffect = useTrail(props.projects.length, {    ////handles fade in effect on school projects
-    //     opacity: 1,
-    //     transform: 'translateY(0)',
-    //     from: {transform: 'translateY(0)'},         
-    // });
-
+    const columnEffect = useTrail(props.projects.length, {    ////handles fade in effect on school projects
+        opacity: 1,
+        transform: 'translateY(0)',
+        from: {opacity: 0},         
+    });
 
     return (
-
-            <Row className={ "justify-content-md-center no-gutters"}>         
-                    { 
-                         props.projects.map(function (item, index) {
-                            return(
-                                <div
-                                    key={item.id}
-                                    // style={props}
-                                    className="columna col-md text-center"
-                                >
-                                    <div className='hover-layer' onMouseOver={hoverEffect} onMouseLeave={hoverEffect}></div>
-                                <div className="project-info">
-                                    <div className="img">
-                                        <img src={item.src} alt=""></img>
-                                    </div>
-                                    <div className="project-web">
-                                        <FontAwesomeIcon icon={faGlobe } > </FontAwesomeIcon>
-                                        <span> </span> {item.title}
-                                    </div>
-                                    <div className="project-tech">
-                                        <FontAwesomeIcon icon={faCode}></FontAwesomeIcon>
-                                        <span> </span> {item.web_techonologies}
-                                    </div>
-                                    
-                                </div>   
-                                </div>
-                        )} 
-                            
-                    ) }
-                </Row>
-
+        <Row className={ "justify-content-md-center no-gutters"}>         
+            {columnEffect.map((prop, index) => (
+                <animated.div
+                    key={props.projects[index].id}
+                    style={prop}
+                    className="columna col-md text-center"
+                >
+                    <div className='hover-layer' onMouseOver={hoverEffect} onMouseLeave={hoverEffect}></div>
+                <div className="project-info">
+                    <div className="img">
+                        <img src={props.projects[index].src} alt=""></img>
+                    </div>
+                    <div className="project-web">
+                        <FontAwesomeIcon icon={faGlobe } > </FontAwesomeIcon>a
+                        <span> </span> {props.projects[index].title}
+                    </div>
+                    <div className="project-tech">
+                        <FontAwesomeIcon icon={faCode}></FontAwesomeIcon>
+                        <span> </span> {props.projects[index].web_techonologies}
+                    </div>
+                    
+                </div>   
+                </animated.div>
+                ))}
+        </Row>
     );
 }
 
